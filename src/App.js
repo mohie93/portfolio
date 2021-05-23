570857c459ac86b4ddd9ccad730baafaf0b1e83a
mohie93 <mohie_93@hotmail.com>
2021-05-23 21:54:48 +0800

    Added typewriter effect

--- src/App.js
+++ src/App.js
@@ -5,22 +5,15 @@ import ContactMe from "./components/ContactMe";
 import Experience from "./components/Experience";
 import Project from "./components/Project";
 import NoMatch from "./components/NoMatch";
-import {useState} from "react";
 
 function App() {
-  let [count, setCount] = useState(0);
-
-  const setCountValue = () => {
-    console.log(count)
-    setCount(0);
-  }
 
   return (
     <div className="App">
       <Navbar/>
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
-          <Route exact path={"/"} component={() => <Introduction setCountValue={setCountValue}/>}/>
+          <Route exact path={"/"} component={Introduction}/>
           <Route exact path={"/projects"} component={Project}/>
           <Route exact path={"/experience"} component={Experience}/>
           <Route exact path={"/contact_me"} component={ContactMe}/>
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,12 +1,18 @@
 import PersonalImage from "./PersonalImage";
+import Typist from "react-typist";
+
 function Introduction() {
   return (
     <div className={'container-fluid text-capitalize mt-2'}>
       <PersonalImage/>
-      <h2 className={'text-center mt-4 '}>< strong className={'greeting'}> Hello</strong>, <strong className={'greeting-color'}>I'm Mohieddin</strong></h2>
-      <h2>An Innovative Software development engineer
+      <div className={'text-center mt-4'}>
+        <Typist avgTypingDelay={50} cursor={{show: false}}>
+          <h2>Hello, I'm <strong className={'greeting-color'}>Mohieddin</strong></h2>
+        </Typist>
+      </div>
+      <h2 className={'mt-2'}>An Innovative Software development engineer
         {/* eslint-disable-next-line react/jsx-no-target-blank */}
-        <a className={'text-decoration-none link-color mt-2'} href='https://supahands.ai'
+        <a className={'text-decoration-none link-color'} href='https://supahands.ai'
            target={'_blank'}><strong> @Supahands</strong></a>
         , offering five years of experience in the software development lifecycle
         from concept through delivery of next-generation applications and customizable solutions.
